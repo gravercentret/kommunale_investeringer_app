089f8b2d19ce0b27a6bfd4939d4ceccadde71b0b
karinaGC1 <karina@gravercentret.dk>
2024-10-03 11:32:36 +0200

    Update Forside.py

--- webapp/Forside.py
+++ webapp/Forside.py
@@ -72,7 +72,7 @@ st.title("Kommunernes og regionernes investeringer")
 
 st.markdown(
     """
-            Gravercentret har sammen med Danwatch undersøgt, hvilke værdipapirer de danske kommuner og regioner har valgt at investere i. \n
+            Gravercentret, Danmarks Center for Undersøgende Journalistik, har sammen med Danwatch undersøgt, hvilke værdipapirer de danske kommuner og regioner har valgt at investere i. \n
             Vi har kortlagt, hvilke værdipapirer der ligger nede i de investeringsfonde og investeringsforeninger, som kommunerne og regionerne har sat deres penge i.\n
             Disse oplysninger har vi sammenholdt med lister over hvilke værdipapirer, der er sortlistet af danske banker og pensionsselskaber samt FN. 
             Herunder kan du se oplysninger fra alle kommuner og regioner – og du kan downloade oplysningerne i Excel-format.
